672fe668439d2d1be4d1b862d7a0672bff5ee210
andrea-sorroche <112932259+andrea-sorroche@users.noreply.github.com>
2022-09-14 17:16:00 +0200

    Update classroom.ts

--- src/app/classroom.ts
+++ src/app/classroom.ts
@@ -1,3 +1,15 @@
+import { Student } from "./student";
+
 export class Classroom {
-    id: number = 0;
+    id: number;
+    students: Student[];
+    img: string;
+    name: string;
+
+    constructor(id: number, students: Student[], img: string, name: string){
+        this.id = id
+        this.students = students
+        this.img = img
+        this.name = name
+    }
 }
