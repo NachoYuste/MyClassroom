c679a46993bfe28b29fc1238c5c98127c729ef12
NachoYuste <ignacioyustel@live.com>
2022-09-14 16:18:28 +0200

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClassListComponent } from './class-list/class-list.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: WelcomeComponent},
+  {path: '/classroom', component: ClassListComponent},
+  {path: '/classroom/:id', component: StudentListComponent},  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ClassListComponent } from './class-list/class-list.component';
+import { StudentListComponent } from './student-list/student-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    WelcomeComponent,
+    ClassListComponent,
+    StudentListComponent
   ],
   imports: [
     BrowserModule,
