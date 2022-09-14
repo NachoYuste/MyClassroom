ca223b8f01744ec58a26d85928b268fb080ad1ef
Jose Gonzalez <jose.gonzalez@insinno.es>
2022-09-14 17:10:32 +0200

    localstorage start

--- src/app/classroom.service.ts
+++ src/app/classroom.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Classroom } from './classroom';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClassroomService {
 
+  classrooms: Classroom[] = [];
   constructor() { }
+
+  getClassrooms(): Classroom[] {
+    if (localStorage.getItem("classrooms")!= undefined){
+      this.classrooms = JSON.parse(localStorage.getItem("classrooms")!);
+    } else {
+      localStorage.setItem("classrooms", JSON.stringify(this.classrooms));
+    }
+    return this.classrooms;
+  }
+
+  getClassroom(id: number): Classroom {
+    this.classrooms = this.getClassrooms();
+    return this.classrooms.find(classroom => classroom.id == id)!;
+  }
+
+  addClassroom(classroom: Classroom){
+    this.classrooms = this.getClassrooms();
+    this.classrooms.push (classroom);
+    localStorage.setItem("classrooms", JSON.stringify(this.classrooms));
+  }
 }
--- src/app/classroom.ts
+++ src/app/classroom.ts
@@ -1,2 +1,3 @@
 export class Classroom {
+    id: number = 0;
 }
