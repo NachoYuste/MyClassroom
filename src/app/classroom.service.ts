8ca2db91c2492b5d3e71e4870f06e951a8228a96
NachoYuste <ignacioyustel@live.com>
2022-09-14 17:47:18 +0200

    classroom service almost complete

--- src/app/classroom.service.ts
+++ src/app/classroom.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Classroom } from './classroom';
+import { Student } from './student';
+import { StudentService } from './student.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,24 @@ export class ClassroomService {
 
   classrooms: Classroom[] = [];
   constructor() { }
+
+  editClassroom(classroom: Classroom, name: string, img: string):Classroom{
+    classroom.name = name;
+    classroom.img = img;
+
+    //Get classrooms
+    this.classrooms = this.getClassrooms();
+
+    //Edit classroom
+    let updateClassroom = this.classrooms.find(updateClassrrom => updateClassrrom.id == classroom.id)!;
+    let index = this.classrooms.indexOf(updateClassroom);
+    this.classrooms[index] = classroom;
+
+    //Update classrooms in local storage
+    localStorage.setItem("classrooms", JSON.stringify(this.classrooms));
+
+    return classroom;
+  }
 
   getClassrooms(): Classroom[] {
     if (localStorage.getItem("classrooms")!= undefined){
@@ -27,5 +47,19 @@ export class ClassroomService {
     this.classrooms = this.getClassrooms();
     this.classrooms.push (classroom);
     localStorage.setItem("classrooms", JSON.stringify(this.classrooms));
+  }
+
+  //Student Service calls
+
+  addStudent(student: Student, studenService: StudentService){
+    studenService.addStudent(student);
+  }
+
+  editStudent(student: Student, studentService: StudentService){
+    studentService.editStudent(student);
+  }
+
+  deleteStudent(student: Student, studentService: StudentService){
+    studentService.delete(student);
   }
 }
